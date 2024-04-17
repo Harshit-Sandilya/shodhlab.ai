78a07cd9b4b1e9da147fda50643dfdc17c45484a
Vrishin Patel <vrishin_patel@outlook.com>
2024-04-17 18:58:57 +0530

    feat: Add action to save contact form submissions to firebase

---
+++ app/contact/actions.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { addDoc, collection, Timestamp } from 'firebase/firestore';
+
+import { firestoreDb } from '@/lib/firebase';
+
+type TConactFormItems = {
+	name: string;
+	email: string;
+	comment: string;
+};
+
+export const submitContactForm = async ({
+	name,
+	email,
+	comment,
+}: TConactFormItems) => {
+	try {
+		const ref = collection(firestoreDb, 'contact_form_submissions');
+		await addDoc(ref, {
+			name,
+			email,
+			comment,
+			submittedAt: Timestamp.now().toDate(),
+		});
+	} catch (err) {
+		console.error(
+			`Failed to add form submission to Firebase. Error details: ${err}`
+		);
+	}
+};
